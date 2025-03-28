819d238d16b7cda0a04bbf47f3308dd6271d36dd
cookie35 <hakyeong35@gmail.com>
2025-03-28 15:47:02 +0900

    Feat: 세이브 및 로드 기능 구현

--- Assets/02.Scripts/Weapon/WeaponData.cs
+++ Assets/02.Scripts/Weapon/WeaponData.cs
@@ -18,7 +18,7 @@ public class WeaponData : ScriptableObject
     public int level;
     public int ability;
 
-    public int ownUpgradePoint;
+    public int ownUpgradePoint; 
 
     public GameObject weaponPrefab;
 }
