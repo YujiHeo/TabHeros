ca0a4e9b720ed1421fa7061b68361e389bd11920
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-03-31 10:38:52 +0900

    [Feat] Inventory 무기 종류별로 팝업되도록 업데이트

--- Assets/02.Scripts/Weapon/WeaponManager.cs
+++ Assets/02.Scripts/Weapon/WeaponManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.UIElements.ToolbarMenu;
 
 public class WeaponManager : Singleton<WeaponManager>
 {
@@ -8,11 +9,16 @@ public class WeaponManager : Singleton<WeaponManager>
 
     private void Start()
     {
-        AddItem(WeaponType.ATKUP);
+        for (int i = 0; i < 5; i++)
+        {
+            AddItem(WeaponType.ATKUP, i);
+        }
+
     }
-    public void AddItem(WeaponType weapon)
+    public void AddItem(WeaponType weapon, int number)
     {
-        WeaponData weaponData = Resources.Load<WeaponData>(weapon.ToString());
+        string assetName = $"{weapon.ToString()} {number}";
+        WeaponData weaponData = Resources.Load<WeaponData>(assetName);
 
         if (weaponData == null)
         {
